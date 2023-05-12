1e1aea620fa1cf9935bb801c46fab3ce04653cf4
林発平 <10223983lin_faping@cn.tre-inc.com>
2023-05-12 13:55:27 +0900

    task1:counter app

---
+++ lib/task1/counter_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            _counter.toString(),
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          IconButton(
+            onPressed: _incrementCount,
+            icon: Icon(
+              Icons.add,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _incrementCount() {
+    setState(() {
+      _counter++;
+    });
+  }
+}
